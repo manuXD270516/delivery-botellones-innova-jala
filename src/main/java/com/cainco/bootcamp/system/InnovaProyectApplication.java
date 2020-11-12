77ea6789cff0b4d5f309be25c5a7215238e1891e
David <davp123.dh@gmail.com>
2020-11-12 18:25:56 -0400

    estructura de proyecto

---
+++ src/main/java/com/cainco/bootcamp/system/InnovaProyectApplication.java
@@ -0,0 +1,13 @@
+package com.cainco.bootcamp.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cainco/bootcamp/system/InnovaProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cainco.bootcamp.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
