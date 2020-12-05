330dc815cfc4416d3d7bd6004b1586bc8b66e480
Manuel J Saavedra Severiche <saavedramanuel100@gmail.com>
2020-12-04 21:35:36 -0400

    cambios parciales

--- target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-insert into zona(nombre, estado) values ('chuquiaguillo',1);
-insert into zona(nombre, estado) values ('villa el carmen',1);
-insert into zona(nombre, estado) values ('miraflores',1);
-insert into zona(nombre, estado) values ('pampahasi',1);
-insert into zona(nombre, estado) values ('buenos aires',0);
